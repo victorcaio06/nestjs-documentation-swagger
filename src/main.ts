7980a43c022a721cccb2e09eae2aab3d376f74e0
Victor Caio <kaioaraujo426@gmail.com>
2022-09-14 09:03:52 -0300

    feat(users): users created

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Documentation com Swagger')
-    .setDescription(
-      'O Swagger (aka OpenApi) é uma biblioteca muito conhecida no universo backend, estando disponível para diversas linguagens e frameworks. Ela gera um site interno no seu backend que descreve, com muitos detalhes, cada endpoint e estrutura de entidades presentes na sua aplicação.',
-    )
+    .setTitle('Documentation with Swagger')
+    .setDescription('User API documentation')
     .setVersion('1.0')
     .addTag('users')
     .build();
