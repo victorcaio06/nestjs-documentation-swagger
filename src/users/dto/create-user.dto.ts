f9f01d6ed971a61e386a63d258e185e2cbc8de02
Victor Caio <kaioaraujo426@gmail.com>
2022-09-14 09:58:38 -0300

    finished documentation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,22 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+  @ApiProperty({
+    example: 'Victor Caio',
+    description: 'Name is used to name the user.',
+  })
+  name: string;
+
+  @ApiProperty({
+    example: 'teste@email.com',
+    description: 'Email is used to login.',
+  })
+  email: string;
+
+  @ApiProperty({
+    example: '967a8fe86251d5139de224cb287d18d866f49698c6b37b0b3a61ca98f29c9bea',
+    description:
+      'Password is used to login. Only the password hash is saved, for login it is necessary to compare it.',
+  })
+  password: string;
+}
